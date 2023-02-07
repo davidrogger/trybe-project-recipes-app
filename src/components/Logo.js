0056d637b4f8be09d3751a914a480f48218be15d
Davíd Roggér <davidrogger.a@gmail.com>
2023-02-07 14:38:07 -0300

    FEAT: logo component at login

---
+++ src/components/Logo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, CardImg } from 'react-bootstrap';
+import logo from '../images/logo.webp';
+
+function Logo() {
+  return (
+    <Container
+      style={ { height: '170px' } }
+    >
+      <CardImg
+        style={ { objectFit: 'cover' } }
+        fluid
+        height={ 170 }
+        src={ logo }
+      />
+    </Container>
+  );
+}
+
+export default Logo;
